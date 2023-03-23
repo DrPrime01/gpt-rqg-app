1e455a60db563747a12273e06ac23778cea120f8
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2023-03-24 00:51:06 +0100

    deleted the apiKey variable and set it directly

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
-const apiKey = process.env.REACT_APP_API_KEY;
-
 export function quoteGenerator(value) {
   return fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
+      Authorization: `Bearer ${import.meta.env.VITE_APP_API_KEY}`,
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
