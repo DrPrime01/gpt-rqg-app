fa6111c8e31b886209a784a8e30d02830afe0fe6
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2023-03-23 16:01:28 +0100

    Created the selectedValue states and functions to manage it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import QuoteBox from "./Components/QuoteBox";
 
 function App() {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const generateQuote = () => {
+    console.log("Quote generated.");
+  };
+  const handleSelectChange = (value) => {
+    setSelectedValue(() => value);
+  };
+
+  useEffect(() => {
+    console.log(selectedValue)
+  }, [selectedValue])
   return (
     <div>
-      
+      <QuoteBox
+        generateQuote={generateQuote}
+        onSelectChange={handleSelectChange}
+        selectedValue={selectedValue}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/QuoteBox.jsx
+++ src/Components/QuoteBox.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 
-function QuoteBox({generateQuote}) {
+function QuoteBox({ generateQuote, onSelectChange, selectedValue }) {
+  const quoteFilterOptions = [
+    "Filter",
+    "Inspiration",
+    "Motivation",
+    "Academic",
+    "Religious",
+    "Masculine",
+    "Feminine",
+  ];
+  const quoteFilterOption = quoteFilterOptions.map((option, i) => {
+    return option === "Filter" ? (
+      <option value="" key={i} disabled defaultValue>
+        {option}
+      </option>
+    ) : (
+      <option value={option} key={i}>
+        {option}
+      </option>
+    );
+  });
+
+  const handleSelectChange = (e) => {
+    onSelectChange(e.target.value);
+  }
   return (
     <div className="col-md-6">
       <div className="h-100 p-5 text-white bg-dark rounded-3">
@@ -10,8 +34,17 @@ function QuoteBox({generateQuote}) {
           mix up the jumbotron look. Then, mix and match with additional
           component themes and more.
         </p>
-        <div className="d-flex justify-between">
-            <button className="btn btn-outline-light" onClick={generateQuote} type="button">Generate</button>
+        <div className="d-flex flex-row justify-content-between">
+          <button
+            className="btn btn-outline-light"
+            onClick={generateQuote}
+            type="button"
+          >
+            Generate
+          </button>
+          <select value={selectedValue} onChange={handleSelectChange}>
+            {quoteFilterOption}
+          </select>
         </div>
       </div>
     </div>
